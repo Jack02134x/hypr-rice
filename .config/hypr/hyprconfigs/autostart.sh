4546735159b66da6d9de04cb29d151b9f906d40f
Jack <brooooooo0707@gmail.com>
2026-05-18 09:47:42 +0530

    .

--- .config/hypr/hyprconfigs/autostart.sh
+++ .config/hypr/hyprconfigs/autostart.sh
@@ -2,41 +2,40 @@
 
 export QT_QPA_PLATFORMTHEME=qt5ct
 export QT_QPA_PLATFORMTHEME=qt6ct
-hyprctl setcursor "Bocchi-The-Cursor" 24
-
+hyprctl setcursor "miku-cursor-linux" 32
 
 # bar/panel
-#./.config/waybar/waybar-switcher/current.sh &
+./.config/waybar/waybar-switcher/current.sh &
 #hyprpanel &
 
 # wallpaper
 # hyprpaper &
-#swww-daemon &
-python /home/jack/.config/hypr/wallpaperslideshow.py &
+#awww-daemon &
+#skwd-daemon &
+# python /home/jack/.config/hypr/wallpaperslideshow.py &
 # swaybg -i ~/.config/hellpaper/wall &
-
+# quickshell &
 
 # Applets
 nm-applet &
-elephant &
 
 #notifications
 swaync &
 
 # applications
-
 Telegram -startintray &
-discord --start-minimized && hyprctl dispatch minimize active &
+vesktop --start-minimized && hyprctl dispatch minimize active &
 # unityhub &
 # protonvpn-app &
 fdm --minimized &
-# heroic &
-# steam -silent &
+heroic &
+steam -silent &
 kdeconnectd &
 
 # hypridle the automatic lock
 hypridle &
 
 # other
+# mpd ~/.config/mpd/mpd.conf &
+mouse_invert &
 wl-paste --watch cliphist store &
-
